540915cd690ebe869a4d2ac341c17717a4a7eb95
DerrickDevelops <derrickdevelops@gmail.com>
2024-04-02 18:26:51 -0400

    Added New Intro
    Created New Intro ("far, far, away).
    
    Corrected Text faded alignment.

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,14 +3,26 @@ import React from "react";
 const MainContent = () => {
     return (
             <div className="wrapper">
-                <div className="scrolling-text">
-                    <h1>STAR WARS</h1>
-                    <h2>Scrolling text</h2>
-                    <p>Hope you have great birthday Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa distinctio porro tempora dolorum quia ut facilis voluptate. Harum ullam asperiores in? In veritatis quas quidem repudiandae ipsum incidunt expedita at? Quas voluptas ad inventore iusto qui deleniti amet, impedit odio?</p>
-                    <p>Hope you have great birthday Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa distinctio porro tempora dolorum quia ut facilis voluptate. Harum ullam asperiores in? In veritatis quas quidem repudiandae ipsum incidunt expedita at? Quas voluptas ad inventore iusto qui deleniti amet, impedit odio?</p>
-                    <p>Hope you have great birthday Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa distinctio porro tempora dolorum quia ut facilis voluptate. Harum ullam asperiores in? In veritatis quas quidem repudiandae ipsum incidunt expedita at? Quas voluptas ad inventore iusto qui deleniti amet, impedit odio?</p>
-
-                </div>
+            <div className="stars"></div>
+	            <section className="intro">
+		            A long time ago, in a galaxy far,<br /> far away....
+	            </section>
+	            <section className="logo">
+		            <img src="logo.png" alt="DevDrawer Star Wars Scroll Screen Mockup" />
+	            </section>
+	                <div id="scroller">
+		                <div id="content">
+			                <p id="title">Episode XXVII</p>
+                                <p id="subtitle">A New Wrinkle</p>
+                                <br />
+                                <p>The DevDrawer was inspired to create something for nerds and geeks alike but unfortunately he missed his own deadline to create something for the day in honor of Star Wars. This episode is to help fix that. So, let's get started.</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. </p>
+                                <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. </p>
+                                <p>Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. </p>
+                                <p>Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. </p>
+                                <p>Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. </p>
+                            </div>
+                        </div>
             </div>
     )
 }
