72c5112c2260542e14b2ea8b483c07195c80cf95
DerrickDevelops <derrickdevelops@gmail.com>
2024-04-01 22:15:37 -0400

    Initial commit

---
+++ src/Content.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MainContent = () => {
+    return (
+            <div className="wrapper">
+                <div className="scrolling-text">
+                    <h1>STAR WARS</h1>
+                    <h2>Scrolling text</h2>
+                    <p>Hope you have great birthday Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa distinctio porro tempora dolorum quia ut facilis voluptate. Harum ullam asperiores in? In veritatis quas quidem repudiandae ipsum incidunt expedita at? Quas voluptas ad inventore iusto qui deleniti amet, impedit odio?</p>
+                    <p>Hope you have great birthday Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa distinctio porro tempora dolorum quia ut facilis voluptate. Harum ullam asperiores in? In veritatis quas quidem repudiandae ipsum incidunt expedita at? Quas voluptas ad inventore iusto qui deleniti amet, impedit odio?</p>
+                    <p>Hope you have great birthday Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa distinctio porro tempora dolorum quia ut facilis voluptate. Harum ullam asperiores in? In veritatis quas quidem repudiandae ipsum incidunt expedita at? Quas voluptas ad inventore iusto qui deleniti amet, impedit odio?</p>
+
+                </div>
+            </div>
+    )
+}
+
+export default MainContent
